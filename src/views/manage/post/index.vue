4e8de79dc3bc08e6ef326e8ea48e2dec13c2556d
fzu221801127 <1647688598@qq.com>
2022-03-03 17:43:21 +0800

    添加恢复按钮

--- src/views/manage/post/index.vue
+++ src/views/manage/post/index.vue
@@ -131,6 +131,7 @@
       >
         <template slot-scope="scope">
           <el-button type="text" @click="deleteStudentById(scope.row.id)">删除</el-button>
+          <el-button type="text" @click="deleteStudentById(scope.row.id)">恢复</el-button>
         </template>
       </el-table-column>
     </el-table>
